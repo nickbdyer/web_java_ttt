6df05e3b0aa7951425e19aca1a2e863394b2e7af
Nick Dyer <nbdyer@gmail.com>
2016-05-23 12:18:15 +0100

    Ties together board methods to faciliate human vs human game play, css tweaks to improve look and feel

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,7 +4,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import uk.nickbdyer.tictactoe.Board;
 import uk.nickbdyer.tictactoe.Game;
-import uk.nickbdyer.tictactoe.Mark;
+import uk.nickbdyer.tictactoe.WebInterface;
 import uk.nickbdyer.tictactoe.players.PlayerFactory;
 import views.html.game;
 import views.html.index;
@@ -16,7 +16,7 @@ import static uk.nickbdyer.tictactoe.GameType.HvsH;
 public class HomeController extends Controller {
 
     private Board board;
-    private Game GAME;
+    private Game currentGame;
 
     public Result index() {
         return ok(index.render("Your new application is ready."));
@@ -26,19 +26,20 @@ public class HomeController extends Controller {
         if (board == null) {
             board = new Board();
         }
-        return ok(game.render("Let's Play!", board.getCells()));
+        return ok(game.render("Let's Play!", board.getCells(), board.getWinningMark(), board.isDraw()));
     }
 
     public Result newgame() {
         board = new Board();
-        GAME = new Game(new PlayerFactory().create(HvsH));
+        currentGame = new Game(new PlayerFactory().create(HvsH, new WebInterface()));
         return redirect("/game");
     }
 
     public Result makeMove() {
         Map<String, String[]> request = request().body().asFormUrlEncoded();
-        board.mark(Integer.valueOf(request.get("position")[0]), Mark.X);
-        return redirect("/game");
+        board.mark(Integer.valueOf(request.get("position")[0]), currentGame.getCurrentPlayer().getMark());
+        currentGame.swapPlayers();
+        return ok(game.render("Let's Play!", board.getCells(), board.getWinningMark(), board.isDraw()));
     }
 
 }
