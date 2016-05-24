a37836fded6e9bd60a9fe150f7ca7cfa590e5874
Nick Dyer <nbdyer@gmail.com>
2016-05-24 16:16:22 +0100

    Fixes errors with rendering of Strings

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -2,9 +2,12 @@ import org.junit.Test;
 import play.twirl.api.Content;
 import uk.nickbdyer.tictactoe.Board;
 
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static uk.nickbdyer.tictactoe.Mark.EMPTY;
 import static uk.nickbdyer.tictactoe.Mark.O;
 import static uk.nickbdyer.tictactoe.Mark.X;
 
@@ -26,34 +29,30 @@ public class ApplicationTest {
 
     @Test
     public void renderGamePage() {
-        Content html = views.html.game.render("Let's Play!", new Board().getCells(), EMPTY, false, false);
+        Content html = views.html.game.render("Let's Play!", new Board().getCells(), null, false);
         assertEquals("text/html", html.contentType());
-        assertTrue(html.body().contains("Play!"));
-        assertTrue(html.body().contains("<td>\n" +
-                "                            <input type=\"hidden\" name=\"position\" value=\"0\">\n" +
-                "                            <input type=\"submit\" value=\"\">\n" +
-                "                        </td>"));
+        assertThat(html.body(), containsString("Let&#x27;s Play!"));
     }
 
     @Test
     public void announcesXWinner() {
-        Content html = views.html.game.render("Let's Play!", new Board().getCells(), X, false, true);
+        Content html = views.html.game.render("Let's Play!", new Board().getCells(), "X is the winner!!", true);
         assertEquals("text/html", html.contentType());
         assertTrue(html.body().contains("X is the winner!!"));
     }
 
     @Test
     public void announcesOWinner() {
-        Content html = views.html.game.render("Let's Play!", new Board().getCells(), O, false, true);
+        Content html = views.html.game.render("Let's Play!", new Board().getCells(), "O is the winner!!", true);
         assertEquals("text/html", html.contentType());
         assertTrue(html.body().contains("O is the winner!!"));
     }
 
     @Test
     public void announcesDraw() {
-        Content html = views.html.game.render("Let's Play!", new Board().getCells(), EMPTY, true, true);
+        Content html = views.html.game.render("Let's Play!", Arrays.asList(X, O, X, O, O, X, X, X, O), "It's a draw!!", true);
         assertEquals("text/html", html.contentType());
-        assertTrue(html.body().contains("It's a draw!!"));
+        assertThat(html.body(), containsString("It&#x27;s a draw!!"));
     }
 
 }
