f6df4f585ba061d0dcfc8cd36fc8df6234e3e02d
Nick Dyer <nbdyer@gmail.com>
2016-06-06 16:33:26 +0100

    Adds board id to view for test purposes, found error with private variables in controller were occasionally getting mixed up with interuptted requests, resolved

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -17,18 +17,16 @@ import java.util.Map;
 
 public class HomeController extends Controller {
 
-    private Board board;
-    private Game currentGame;
     private WebInterface ui = new WebInterface();
     private Map<String, Board> boardMap = new HashMap<>();
     private Map<String, Game> gameMap = new HashMap<>();
 
     public Board getBoard() {
-        return board;
+        return boardMap.get(session("board_id"));
     }
 
     public Game getGame() {
-        return currentGame;
+        return gameMap.get(session("game_id"));
     }
 
     public Result index() {
@@ -40,31 +38,30 @@ public class HomeController extends Controller {
     }
 
     public Result newGame() {
+        session().clear();
         return redirect("/chooseGame");
     }
 
     public Result chooseGame() {
         Map<String, String[]> request = request().body().asFormUrlEncoded();
-        board = new Board();
-        currentGame = new Game(new PlayerFactory(ui).create(GameType.values()[Integer.valueOf(request.get("gameType")[0])]));
+        Board board = new Board();
+        Game game = new Game(new PlayerFactory(ui).create(GameType.values()[Integer.valueOf(request.get("gameType")[0])]));
         boardMap.put(Integer.toString(board.hashCode()), board);
-        gameMap.put(Integer.toString(currentGame.hashCode()), currentGame);
-        session("game_id", Integer.toString(currentGame.hashCode()));
+        gameMap.put(Integer.toString(game.hashCode()), game);
+        session("game_id", Integer.toString(game.hashCode()));
         session("board_id", Integer.toString(board.hashCode()));
         return redirect("/play");
     }
 
     public Result play(Option<Integer> position) {
-        board = boardMap.get(session("board_id"));
-        currentGame = gameMap.get(session("game_id"));
-        System.out.println(session());
-        System.out.println(boardMap.toString());
+        Board board = boardMap.get(session("board_id"));
+        Game currentGame = gameMap.get(session("game_id"));
         if (position.isDefined() && !ui.nextMoveIsValid(board, currentGame.getCurrentPlayer().choosePosition(board))) {
             ui.makeNextMove(currentGame, board, position.get());
-            return ok(game.render("Let's Play!", board.getCells(), ui.endGame(board), currentGame.isOver(board)));
+            return ok(game.render("Let's Play!", board.getCells(), ui.endGame(board), currentGame.isOver(board), Integer.toString(board.hashCode())));
         }
         ui.makeNextMove(currentGame, board, currentGame.getCurrentPlayer().choosePosition(board));
-        return ok(game.render("Let's Play!", board.getCells(), ui.endGame(board), currentGame.isOver(board)));
+        return ok(game.render("Let's Play!", board.getCells(), ui.endGame(board), currentGame.isOver(board), Integer.toString(board.hashCode())));
     }
 
 }
